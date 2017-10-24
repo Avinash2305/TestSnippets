477ed630883873e31d5bb988452b665d406dfbf9
venkatramanan <venkatramanan@gmail.com>
2017-10-24 13:41:19 +0530

    Update CustomLogHandler.java

--- CustomLogHandler.java
+++ CustomLogHandler.java
@@ -1,14 +1,22 @@
-package com.ns.ems.server.log;
+package com.adventnet.management.log;
 
 import com.adventnet.management.log.Log4jHandler;
 import org.apache.log4j.xml.DOMConfigurator;
+import com.adventnet.nms.util.RunProcessInterface;
 import java.net.URL;
 
-public class CustomLogHandler extends Log4jHandler {
-
-	public void configure(URL url)
-	  {
-		DOMConfigurator.configureAndWatch(url.getFile(),300000);
-	  }
-
+public class CustomLogHandler extends Log4jHandler implements RunProcessInterface {
+        public void configure(URL url)
+        {   
+                DOMConfigurator.configureAndWatch(url.getFile(),30000);
+        }   
+        public void callMain(String args[]){
+                try{
+                        System.out.println("Dummy implementation. Main work is for ConfigureAndWatch");
+                } catch(Exception e){ 
+                        e.printStackTrace();
+                }   
+        }   
+        public boolean isInitialized() {return true;}
+        public void shutDown() {}
 }
