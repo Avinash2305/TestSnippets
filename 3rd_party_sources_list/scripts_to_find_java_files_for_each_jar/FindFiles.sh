eac8a525ecb4778025f51d44026d28355c72ff23
venkatramanan <venkatramanan@gmail.com>
2017-12-23 20:48:03 -0500

    3rd party project sources - script files
    3rd party project sources - script files

---
+++ 3rd_party_sources_list/scripts_to_find_java_files_for_each_jar/FindFiles.sh
@@ -0,0 +1,104 @@
+echo "running AdventNetARUtils.sh"
+nohup sh AdventNetARUtils.sh >> ../grep_matches_files/AdventNetARUtils.out
+echo "running AdventNetAgentUIUtilities.sh"
+nohup sh AdventNetAgentUIUtilities.sh >> ../grep_matches_files/AdventNetAgentUIUtilities.out
+echo "running AdventNetBeanBuilder.sh"
+nohup sh AdventNetBeanBuilder.sh >> ../grep_matches_files/AdventNetBeanBuilder.out
+echo "running AdventNetBuilderDeployment.sh"
+nohup sh AdventNetBuilderDeployment.sh >> ../grep_matches_files/AdventNetBuilderDeployment.out
+echo "running AdventNetBuilderMigration.sh"
+nohup sh AdventNetBuilderMigration.sh >> ../grep_matches_files/AdventNetBuilderMigration.out
+echo "running AdventNetCCLLibrary.sh"
+nohup sh AdventNetCCLLibrary.sh >> ../grep_matches_files/AdventNetCCLLibrary.out
+echo "running AdventNetCLI.sh"
+nohup sh AdventNetCLI.sh >> ../grep_matches_files/AdventNetCLI.out
+echo "running AdventNetCLIClient.sh"
+nohup sh AdventNetCLIClient.sh >> ../grep_matches_files/AdventNetCLIClient.out
+echo "running AdventNetCorbaSimulator.sh"
+nohup sh AdventNetCorbaSimulator.sh >> ../grep_matches_files/AdventNetCorbaSimulator.out
+echo "running AdventNetDMPModels.sh"
+nohup sh AdventNetDMPModels.sh >> ../grep_matches_files/AdventNetDMPModels.out
+echo "running AdventNetIDLtoJObjects.sh"
+nohup sh AdventNetIDLtoJObjects.sh >> ../grep_matches_files/AdventNetIDLtoJObjects.out
+echo "running AdventNetIdlToXml.sh"
+nohup sh AdventNetIdlToXml.sh >> ../grep_matches_files/AdventNetIdlToXml.out
+echo "running AdventNetJavaAgentComponents.sh"
+nohup sh AdventNetJavaAgentComponents.sh >> ../grep_matches_files/AdventNetJavaAgentComponents.out
+echo "running AdventNetJmxAgent.sh"
+nohup sh AdventNetJmxAgent.sh >> ../grep_matches_files/AdventNetJmxAgent.out
+echo "running AdventNetJta.sh"
+nohup sh AdventNetJta.sh >> ../grep_matches_files/AdventNetJta.out
+echo "running AdventNetNPrevalent.sh"
+nohup sh AdventNetNPrevalent.sh >> ../grep_matches_files/AdventNetNPrevalent.out
+echo "running AdventNetOPExtn.sh"
+nohup sh AdventNetOPExtn.sh >> ../grep_matches_files/AdventNetOPExtn.out
+echo "running AdventNetProBeans.sh"
+nohup sh AdventNetProBeans.sh >> ../grep_matches_files/AdventNetProBeans.out
+echo "running AdventNetProBeansBeanInfo.sh"
+nohup sh AdventNetProBeansBeanInfo.sh >> ../grep_matches_files/AdventNetProBeansBeanInfo.out
+echo "running AdventNetRuntimeUtilities.sh"
+nohup sh AdventNetRuntimeUtilities.sh >> ../grep_matches_files/AdventNetRuntimeUtilities.out
+echo "running AdventNetSAS.sh"
+nohup sh AdventNetSAS.sh >> ../grep_matches_files/AdventNetSAS.out
+echo "running AdventNetSNMPAgentTools.sh"
+nohup sh AdventNetSNMPAgentTools.sh >> ../grep_matches_files/AdventNetSNMPAgentTools.out
+echo "running AdventNetSUMClient.sh"
+nohup sh AdventNetSUMClient.sh >> ../grep_matches_files/AdventNetSUMClient.out
+echo "running AdventNetSUMServer.sh"
+nohup sh AdventNetSUMServer.sh >> ../grep_matches_files/AdventNetSUMServer.out
+echo "running AdventNetSnmp.sh"
+nohup sh AdventNetSnmp.sh >> ../grep_matches_files/AdventNetSnmp.out
+echo "running AdventNetSnmpAgent.sh"
+nohup sh AdventNetSnmpAgent.sh >> ../grep_matches_files/AdventNetSnmpAgent.out
+echo "running AdventNetSnmpBeanInfo.sh"
+nohup sh AdventNetSnmpBeanInfo.sh >> ../grep_matches_files/AdventNetSnmpBeanInfo.out
+echo "running AdventNetSnmpDistributedAPI.sh"
+nohup sh AdventNetSnmpDistributedAPI.sh >> ../grep_matches_files/AdventNetSnmpDistributedAPI.out
+echo "running AdventNetSnmpV3USM.sh"
+nohup sh AdventNetSnmpV3USM.sh >> ../grep_matches_files/AdventNetSnmpV3USM.out
+echo "running AdventNetTL1.sh"
+nohup sh AdventNetTL1.sh >> ../grep_matches_files/AdventNetTL1.out
+echo "running AdventNetTL1Agent.sh"
+nohup sh AdventNetTL1Agent.sh >> ../grep_matches_files/AdventNetTL1Agent.out
+echo "running AdventNetTL1AgentTools.sh"
+nohup sh AdventNetTL1AgentTools.sh >> ../grep_matches_files/AdventNetTL1AgentTools.out
+echo "running AdventNetTL1Tools.sh"
+nohup sh AdventNetTL1Tools.sh >> ../grep_matches_files/AdventNetTL1Tools.out
+echo "running AdventNetTcsToXmlConvertor.sh"
+nohup sh AdventNetTcsToXmlConvertor.sh >> ../grep_matches_files/AdventNetTcsToXmlConvertor.out
+echo "running AdventNetTftp.sh"
+nohup sh AdventNetTftp.sh >> ../grep_matches_files/AdventNetTftp.out
+echo "running AdventNetThemeManager.sh"
+nohup sh AdventNetThemeManager.sh >> ../grep_matches_files/AdventNetThemeManager.out
+echo "running AdventNetToolsUtilities.sh"
+nohup sh AdventNetToolsUtilities.sh >> ../grep_matches_files/AdventNetToolsUtilities.out
+echo "running AdventNetUI.sh"
+nohup sh AdventNetUI.sh >> ../grep_matches_files/AdventNetUI.out
+echo "running AdventNetUIBeanInfo.sh"
+nohup sh AdventNetUIBeanInfo.sh >> ../grep_matches_files/AdventNetUIBeanInfo.out
+echo "running AdventNetUpdateManager.sh"
+nohup sh AdventNetUpdateManager.sh >> ../grep_matches_files/AdventNetUpdateManager.out
+echo "running AdventNetUpdateManagerInstaller.sh"
+nohup sh AdventNetUpdateManagerInstaller.sh >> ../grep_matches_files/AdventNetUpdateManagerInstaller.out
+echo "running AdventNetUtils.sh"
+nohup sh AdventNetUtils.sh >> ../grep_matches_files/AdventNetUtils.out
+echo "running AdventNetUtilsBeanInfo.sh"
+nohup sh AdventNetUtilsBeanInfo.sh >> ../grep_matches_files/AdventNetUtilsBeanInfo.out
+echo "running AdventNetWebNmsAgent.sh"
+nohup sh AdventNetWebNmsAgent.sh >> ../grep_matches_files/AdventNetWebNmsAgent.out
+echo "running ApiUtils.sh"
+nohup sh ApiUtils.sh >> ../grep_matches_files/ApiUtils.out
+echo "running ManagementClient.sh"
+nohup sh ManagementClient.sh >> ../grep_matches_files/ManagementClient.out
+echo "running ManagementServer.sh"
+nohup sh ManagementServer.sh >> ../grep_matches_files/ManagementServer.out
+echo "running NarPackager.sh"
+nohup sh NarPackager.sh >> ../grep_matches_files/NarPackager.out
+echo "running SNMPConfigurationTool.sh"
+nohup sh SNMPConfigurationTool.sh >> ../grep_matches_files/SNMPConfigurationTool.out
+echo "running SNMPDebugger.sh"
+nohup sh SNMPDebugger.sh >> ../grep_matches_files/SNMPDebugger.out
+echo "running debugger.sh"
+nohup sh debugger.sh >> ../grep_matches_files/debugger.out
+echo "running xmojo.sh"
+nohup sh xmojo.sh >> ../grep_matches_files/xmojo.out
