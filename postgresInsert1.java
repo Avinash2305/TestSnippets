16f836b8972aa3e8a904b8357dc5beaa83aa5a76
venkatramanan <venkatramanan@gmail.com>
2018-04-04 10:50:12 +0530

    Update postgresInsert1.java

--- postgresInsert1.java
+++ postgresInsert1.java
@@ -2,16 +2,18 @@ import java.sql.*;
 public class postgresInsert1
 {
 	public static void main(String args[]) {
-		String url = "jdbc:postgresql://localhost/testdb";
+		String url = "jdbc:postgresql://localhost/l3db";
 		String drivername= "org.postgresql.Driver";
+		String userName = "postgres";
+		String password = "postgres";
 		String str="";
 		Statement stmt = null;
 		Connection conn = null;
 		try{
 			Class.forName(drivername);
-			conn = DriverManager.getConnection(url, "postgres", "postgres" );
+			conn = DriverManager.getConnection(url, userName, password );
 			stmt=conn.createStatement();
-			str= "create table users1 (emp citext, pass text);";
+			str= "create table users1 (emp text, pass text);";
 			stmt.execute(str);
 			str= "insert into users1 values('b', 'cddd');";
 			stmt.execute(str);
