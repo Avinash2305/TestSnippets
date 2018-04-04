2d6e74165ad817a1211ddf27c8b58f502ab06a94
venkatramanan <venkatramanan@gmail.com>
2018-04-04 11:24:17 +0530

    MySQLInsert.java
    MySQLInsert.java

--- MySQLInsert.java
+++ MySQLInsert.java
@@ -1,38 +1,27 @@
-import java.io.*;
-import java.lang.*;
 import java.sql.*;
-
 public class MySQLInsert
 {
-	public static void main(String args[])
-	{
-		//String url = "jdbc:mysql://192.168.16.56/test";
-		String url = "jdbc:mysql://localhost/nms5?jdbcCompliantTruncation=false";
-		String drivername= "org.gjt.mm.mysql.Driver";
+	public static void main(String args[]) {
+		String url = "jdbc:mysql://localhost/WebNmsDB";
+		String drivername= "com.mysql.jdbc.Driver";
+		String userName = "root";
+		String password = "";
 		String str="";
-
 		Statement stmt = null;
 		Connection conn = null;
 		try{
 			Class.forName(drivername);
-			conn = DriverManager.getConnection(url, "root", "" );
+			conn = DriverManager.getConnection(url, userName, password );
 			stmt=conn.createStatement();
-			str= "create table users3 (emp varchar(3));";
+			str= "create table users1 (emp text, pass text);";
 			stmt.execute(str);
-			str= "insert into users3 values('1234');";
+			str= "insert into users1 values('b', 'cddd');";
 			stmt.execute(str);
 		}
-		catch (Exception exp)
-		{exp.printStackTrace();}
+		catch (Exception exp) {exp.printStackTrace();}
 		finally{
-			try{
-				if(conn !=null)
-					conn.close();
-				if(stmt != null)
-					stmt.close();
-			}
-			catch (Exception e){}
+			try{ if(conn !=null) {conn.close();}
+				if(stmt != null) {stmt.close();}
+			} catch (Exception e){}
 		}
-
-	}
-}
+	} }
